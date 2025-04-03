c8143a64ed1b048a2c64cdc02e56f8ea5536f1e4
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-04-03 08:19:30 -0300

    FindById tests

--- src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dsmovie.controllers;
 
 import org.json.JSONException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 public class MovieControllerRA {
+	
+	private Long existingMovieId, nonExistingMovieId;
+	
+	@BeforeEach
+	public void setUp() {
+		baseURI = "http://localhost:8080";
+	}
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
@@ -15,10 +27,27 @@ public class MovieControllerRA {
 	
 	@Test
 	public void findByIdShouldReturnMovieWhenIdExists() {		
+		existingMovieId = 2L;
+		
+		given()
+			.get("/movies/{id}", existingMovieId)
+		.then()
+			.statusCode(200)
+			.body("id", is(2))
+			.body("title", equalTo("Venom: Tempo de Carnificina"))
+			.body("score", is(3.3F))
+			.body("count", is(3))
+			.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/vIgyYkXkg6NC2whRbYjBD7eb3Er.jpg"));
 	}
 	
 	@Test
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+		nonExistingMovieId = 500L;
+		
+		given()
+			.get("/movies/{id}", nonExistingMovieId)
+		.then()
+			.statusCode(404);
 	}
 	
 	@Test
